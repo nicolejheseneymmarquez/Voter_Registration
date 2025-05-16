6361db671121cb00257b9cdde99ddb1e807ff731
nicolejheseneymmarquez <nicolejheseneymanalo@gmail.com>
2025-05-16 19:52:46 +0800

    Voter-Test Underage TEST 4

--- test_voter_checker.py
+++ test_voter_checker.py
@@ -8,7 +8,6 @@ class TestVoterEligibility(unittest.TestCase):
     def test_valid_voter(self):
         self.assertTrue(is_eligible_to_vote(18, True))
 
-
     def test_valid_voter_underage(self):
         self.assertTrue(is_eligible_to_vote(17, False))
-
+        
